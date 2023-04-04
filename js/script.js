66ef5b969e4ab5aad70eac86b19f3c65d56ba5aa
Seyi Samuel <seyi.samuel140@gmail.com>
2023-04-04 20:03:12 +0100

    The helper function named createTodoItem was just written to aaid the yet to be written addTodo function effectively.

--- js/script.js
+++ js/script.js
@@ -3,3 +3,30 @@ const addTodoBtn = document.querySelector(".todo-btn");
 const todoList = document.querySelector(".todo-list");
 
 const editItem = null;
+
+const createTodoItem = (item) => {
+    let divTodo = document.createElement("div");
+    divTodo.classList.add("todo-div");
+
+    let itemLi = document.createElement("li");
+    itemLi.classList.add("todo-task");
+    itemLi.innerText = item;
+    // add local storage check to this line later
+    // add save to local storage to this line later
+    divTodo.append(itemLi);
+
+    let editBtn = document.createElement("button");
+    editBtn.classList.add("edit-btn");
+    editBtn.innerHTML = "edit";
+    divTodo.append(editBtn);
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("delete-btn");
+    deleteBtn.innerHTML = "delete";
+    divTodo.append(deleteBtn);
+    deleteBtn.addEventListener("click", () => {
+        divTodo.remove();
+        // todoList.removeChild(divTodo);
+        // add save to local storage to this line later
+    });
+};
