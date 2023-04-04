3cd7152be6c95beeb6f7c2ff77e523a7d747bad1
Seyi Samuel <seyi.samuel140@gmail.com>
2023-04-04 19:39:43 +0100

    Getting the dom selectors needed and assigning it to variables

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const inputTodo = document.querySelector(".todo-input");
+const addTodoBtn = document.querySelector(".todo-btn");
+const todoList = document.querySelector(".todo-list");
+
+const editItem = null;
